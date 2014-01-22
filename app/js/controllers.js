55c36a076afb25ea2b33d09cc17d272d7e934d90
Bryan Conrad <bkconrad@gmail.com>
2014-01-22 13:51:16 -0800

    add column definitions

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,22 +9,49 @@ phonecatApp.controller('StatsCtrl', function ($scope, $http) {
 		showFooter: true,
 		showHeader: true,
 		pagingOptions: {
-			// pageSizes: list of available page sizes.
 			pageSizes: [5, 10, 40], 
-			//pageSize: currently selected page size. 
 			pageSize: 40,
-			//currentPage: the uhm... current page.
 			currentPage: 1
 		},	
 		enablePaging: true,
+		multiSelect: false,
 		totalServerItems: 'total',
+		columnDefs: [
+			{field: 'player_name', displayName: 'Player Name', width: '100'},
+			{field: 'kill_death_ratio', displayName: 'KDR'},
+			{field: 'kill_count', displayName: 'Kills'},
+			{field: 'death_count', displayName: 'Deaths'},
+			{field: 'suicide_count', displayName: 'Suicides'},
+			{field: 'game_count', displayName: 'Games'},
+			{field: 'win_count', displayName: 'Wins'},
+			{field: 'lose_count', displayName: 'Losses'},
+			{field: 'tie_count', displayName: 'Ties'},
+			{field: 'points', displayName: 'Points'},
+			{field: 'flag_drops', displayName: 'Flags Dropped'},
+			{field: 'flag_pickups', displayName: 'Flags Picked Up'},
+			{field: 'flag_returns', displayName: 'Flags Returned'},
+			{field: 'flag_scores', displayName: 'Flags Scored'},
+			{field: 'teleport_uses', displayName: 'Times Teleported'},
+			{field: 'asteroid_crashes', displayName: 'Asteroid Mishaps'},
+			{field: 'switched_team_count', displayName: 'Team Switches'},
+			{field: 'last_played', displayName: 'Last Played'}
+		],
 		data: 'stats'
 	};
 
 	function getData(page, per_page) {
+		var i;
+		var row;
 		$http.get('stats.php', { params: { page: page, per_page: per_page }}).success(function(data) {
 			$scope.total = data.count;
 			$scope.stats = data;
+
+			// for(i in data) {
+			// 	row = data[i];
+			// 	row.games_total = row.
+			// }
+
+			console.log(data);
 		}).
 		error(function(data, status, headers, config) {
 			console.log(status);
