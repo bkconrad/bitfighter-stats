fa852c2d894e199ed833cde5894eac6a9a2b3915
Bryan Conrad <bkconrad@gmail.com>
2014-01-25 21:05:56 -0800

    massive restyle

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -77,31 +77,33 @@ bfstats.controller('StatsCtrl', function ($scope, $http) {
 
 	$scope.options = {
 		enableColumnResize: true,
-		showFooter: true,
+		showColumnMenu: true,
+		showFilter: true,
+		// showFooter: true,
 		showHeader: true,
 		afterSelectionChange: function(row, event) {
 			$scope.selectedPlayer = row.entity;
 		},
 		multiSelect: false,
 		columnDefs: [
-			{field: 'player_name',         displayName: 'Player Name', width: '100'},
-			{field: 'kill_death_ratio',    displayName: 'KDR'                      },
-			{field: 'kill_count',          displayName: 'Kills'                    },
-			{field: 'death_count',         displayName: 'Deaths'                   },
-			{field: 'suicide_count',       displayName: 'Suicides'                 },
-			{field: 'game_count',          displayName: 'Games'                    },
-			{field: 'win_count',           displayName: 'Wins'                     },
-			{field: 'lose_count',          displayName: 'Losses'                   },
-			{field: 'tie_count',           displayName: 'Ties'                     },
-			{field: 'points',              displayName: 'Points'                   },
-			{field: 'flag_drops',          displayName: 'Flags Dropped'            },
-			{field: 'flag_pickups',        displayName: 'Flags Picked Up'          },
-			{field: 'flag_returns',        displayName: 'Flags Returned'           },
-			{field: 'flag_scores',         displayName: 'Flags Scored'             },
-			{field: 'teleport_uses',       displayName: 'Times Teleported'         },
-			{field: 'asteroid_crashes',    displayName: 'Asteroid Mishaps'         },
-			{field: 'switched_team_count', displayName: 'Team Switches'            },
-			{field: 'last_played',         displayName: 'Last Played'              }
+			{field: 'player_name',         visible: true, displayName: 'Player Name', width: '100'},
+			{field: 'kill_death_ratio',    visible: true, displayName: 'KDR'                      },
+			{field: 'kill_count',          visible: true, displayName: 'Kills'                    },
+			{field: 'death_count',         visible: true, displayName: 'Deaths'                   },
+			{field: 'suicide_count',       visible: false, displayName: 'Suicides'                 },
+			{field: 'game_count',          visible: true, displayName: 'Games'                    },
+			{field: 'win_count',           visible: true, displayName: 'Wins'                     },
+			{field: 'lose_count',          visible: true, displayName: 'Losses'                   },
+			{field: 'tie_count',           visible: false, displayName: 'Ties'                     },
+			{field: 'points',              visible: false, displayName: 'Points'                   },
+			{field: 'flag_drops',          visible: false, displayName: 'Flags Dropped'            },
+			{field: 'flag_pickups',        visible: false, displayName: 'Flags Picked Up'          },
+			{field: 'flag_returns',        visible: false, displayName: 'Flags Returned'           },
+			{field: 'flag_scores',         visible: false, displayName: 'Flags Scored'             },
+			{field: 'teleport_uses',       visible: false, displayName: 'Times Teleported'         },
+			{field: 'asteroid_crashes',    visible: false, displayName: 'Asteroid Mishaps'         },
+			{field: 'switched_team_count', visible: false, displayName: 'Team Switches'            },
+			{field: 'last_played',         visible: false, displayName: 'Last Played'              }
 		],
 		data: 'stats'
 	};
