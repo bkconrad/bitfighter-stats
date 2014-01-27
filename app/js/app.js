f6587992076b91633ee66dea742e4705c092febd
Bryan Conrad <bkconrad@gmail.com>
2014-01-27 07:04:39 -0800

    split views into partials

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,8 @@ angular.module('bfstats', [
   'bfstats.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  // $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/games', {templateUrl: 'app/partials/games.html', controller: 'GamesCtrl'});
+  $routeProvider.otherwise({templateUrl: 'app/partials/stats.html', controller: 'StatsCtrl'});
   // $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   // $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
