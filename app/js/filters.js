1b45e735b583d41ed0ab56788492641e3259b51e
Bryan Conrad <bkconrad@gmail.com>
2014-01-28 23:14:04 -0800

    add stat filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,4 +4,22 @@ angular.module('bfstats.filters', [])
 			var val = parseFloat(input) || 0;
 			return (Math.round(val * 10000) / 100).toFixed(2) + '%';
 		};
-	});
+	})
+	.filter('stat', function() {
+		return function(input) {
+			var val = parseFloat(input) || 0;
+			var output;
+			if(val.toString().indexOf(input) === 0) {
+				if(Math.floor(val) === val) {
+					// no decimal part
+					output = val.toString();
+				} else {
+					output = val.toFixed(2);
+				}
+			} else {
+				output = input;
+			}
+			return output;
+		};
+	})
+	;
