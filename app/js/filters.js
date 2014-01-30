8f36aa221a079a07fe263dbc4a690c1be40fea9f
Bryan Conrad <bkconrad@gmail.com>
2014-01-30 06:43:39 -0800

    add stat name filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -22,6 +22,17 @@ angular.module('bfstats.filters', [])
 			return output;
 		};
 	})
+	.filter('statName', function() {
+		return function(input) {
+			return input
+				.split('_')
+				.map(function(x) {
+					return x.charAt(0).toUpperCase() + x.slice(1);
+				})
+				.join(' ')
+				;
+		};
+	})
 	.filter('monthYear', function() {
 		return function(input) {
 			return moment(input).format('MMM YYYY');
