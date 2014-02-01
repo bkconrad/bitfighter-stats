88e7c737ba63f50e7cfae40798da552544550390
Bryan Conrad <bkconrad@gmail.com>
2014-02-01 12:30:36 -0800

    cache players_per_game

--- players_per_game.php
+++ players_per_game.php
@@ -3,34 +3,37 @@
 date_default_timezone_set('America/Denver');
 
 require "db_functions.php";
+require "lib.php";
 
-$query = "
+echo cache('players_per_game', 60 * 60 * 24, function() {
+	$query = "
 
-select tmp.day_date, AVG(tmp.ppg) as players_per_game, tmp2.unique_player_count as unique_player_count
-from (
-    select sp.stats_game_id, date(sp.insertion_date) as day_date, count(sp.player_name) as ppg
-    from stats_player sp
-    where is_robot = 0
-    group by sp.stats_game_id, day_date
-) tmp,
-(
-    select date(insertion_date) as day_date, count(distinct player_name) as unique_player_count
-    from stats_player
-    where is_robot = 0
-    group by day_date
-) tmp2
-where tmp.day_date = tmp2.day_date AND tmp.day_date between sysdate() - interval 120 day and sysdate() - interval 1 day
-group by tmp.day_date
+	select tmp.day_date, AVG(tmp.ppg) as players_per_game, tmp2.unique_player_count as unique_player_count
+	from (
+	    select sp.stats_game_id, date(sp.insertion_date) as day_date, count(sp.player_name) as ppg
+	    from stats_player sp
+	    where is_robot = 0
+	    group by sp.stats_game_id, day_date
+	) tmp,
+	(
+	    select date(insertion_date) as day_date, count(distinct player_name) as unique_player_count
+	    from stats_player
+	    where is_robot = 0
+	    group by day_date
+	) tmp2
+	where tmp.day_date = tmp2.day_date AND tmp.day_date between sysdate() - interval 120 day and sysdate() - interval 1 day
+	group by tmp.day_date
 
-";
+	";
 
-$mysqli = connect_to_db();
-$result = mysqli_query($mysqli, $query);
-$result or die(mysqli_error($mysqli));
+	$mysqli = connect_to_db();
+	$result = mysqli_query($mysqli, $query);
+	$result or die(mysqli_error($mysqli));
 
-$data = array();
-while($row = mysqli_fetch_assoc($result)) {
-  array_push($data, $row);
-}
+	$data = array();
+	while($row = mysqli_fetch_assoc($result)) {
+	  array_push($data, $row);
+	}
 
-echo json_encode($data);
+	return json_encode($data);
+});
