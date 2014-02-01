cad22171ff5ce73be59bee77602ab4756c25a1be
Bryan Conrad <bkconrad@gmail.com>
2014-02-01 12:29:01 -0800

    cache game times

--- game_times.php
+++ game_times.php
@@ -2,25 +2,28 @@
 
 date_default_timezone_set('America/Denver');
 
+require "lib.php";
 require "db_functions.php";
 
-$query = "
-SELECT
-	COUNT(1) AS `count`,
-	DAYOFWEEK( `insertion_date` ) AS `day`,
-	HOUR( `insertion_date` ) AS `hour` 
-FROM  `stats_game` 
-GROUP BY CONCAT( DAYOFWEEK( insertion_date ) ,  ' ', HOUR( insertion_date ) ) 
-";
-
-$mysqli = connect_to_db();
-$result = mysqli_query($mysqli, $query);
-$result or die(mysqli_error($mysqli));
-
-$data = array();
-while($row = mysqli_fetch_assoc($result)) {
-  array_push($data, $row);
-}
-
 header('Content-Type: application/json');
-echo json_encode($data);
+echo cache('game_times', 60 * 60 * 24, function() {
+	$query = "
+	SELECT
+		COUNT(1) AS `count`,
+		DAYOFWEEK( `insertion_date` ) AS `day`,
+		HOUR( `insertion_date` ) AS `hour` 
+	FROM  `stats_game` 
+	GROUP BY CONCAT( DAYOFWEEK( insertion_date ) ,  ' ', HOUR( insertion_date ) ) 
+	";
+
+	$mysqli = connect_to_db();
+	$result = mysqli_query($mysqli, $query);
+	$result or die(mysqli_error($mysqli));
+
+	$data = array();
+	while($row = mysqli_fetch_assoc($result)) {
+	  array_push($data, $row);
+	}
+
+	return json_encode($data);
+});
